6266c24640b2e34855f1b68c88e9c164192a3920
CoinSwap <79321995+coinswapapp@users.noreply.github.com>
2021-03-22 19:43:38 -0400

    Delete ICoinSwapCallee.sol

--- interfaces/ICoinSwapCallee.sol
+++
@@ -1,6 +0,0 @@
-//SPDX-License-Identifier: TBD
-pragma solidity >=0.7.4;
-
-interface ICoinSwapCallee {
-    function coinswapCall(address sender, uint amount0,uint amount1, bytes calldata data) external;
-}
